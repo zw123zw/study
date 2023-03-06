afaa6c92794a3b41b3141219cabcbd5174df5645
zw123zw <zw18674069215@163.com>
2023-03-06 23:20:32 +0800

    mitt学习

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,7 +3,32 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import mitt from 'mitt'
+import type { Emitter } from 'mitt'
+
+type Events = {
+  foo: string
+  bar?: number
+  resize: {
+    detail: {
+      width: number
+      height: number
+    }
+  }
+}
+
+const emitter: Emitter<Events> = mitt<Events>()
+console.log(emitter)
+
+emitter.on('foo', e => console.log('foo', e))
+emitter.on('bar', e => console.log('bar', e))
+emitter.on('resize', e => console.log('resize', e))
+setTimeout(() => {
+  emitter.emit('foo', '213')
+  emitter.emit('bar', 111)
+  emitter.all.clear()
+}, 2000)
 </script>
 
 <style lang="scss" scoped></style>
