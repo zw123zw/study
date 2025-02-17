775155ca56f3b44548689504852f0021eeba9215
张威 <wei.zhang@tophant.com>
2025-02-17 20:59:51 +0800

    useFetch学习

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,10 +6,12 @@
 </template>
 
 <script setup lang="ts">
+const description = ref("My amazing site.");
+
 useHead({
-  // or as a function
   titleTemplate: (productCategory) => {
     return productCategory ? `${productCategory} - Site Title` : "Site Title";
   },
+  meta: [{ name: "description", content: description }],
 });
 </script>
--- pages/test/index.vue
+++ pages/test/index.vue
@@ -2,5 +2,33 @@
   <div>
     <div>test页面</div>
     <nuxt-link to="/">home</nuxt-link>
+    <div @click="addTodo">addTodo</div>
   </div>
 </template>
+
+<script setup lang="ts">
+const { data } = await useFetch("/api/data");
+console.log(data.value);
+
+const headers = useRequestHeaders(["cookie"]);
+console.log(headers.value);
+
+async function addTodo() {
+  const todo = await $fetch("/api/todos", {
+    method: "POST",
+    body: {
+      // My todo data
+    },
+  });
+}
+const { data: discounts, status } = await useAsyncData('cart-discount', async () => {
+  const [coupons, offers] = await Promise.all([
+    $fetch('/cart/coupons'),
+    $fetch('/cart/offers')
+  ])
+  return { coupons, offers }
+})
+console.log(discounts.value);
+console.log(status.value);
+
+</script>
